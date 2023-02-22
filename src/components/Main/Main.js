c3062784333ffafe096273ddb44305107ff3351e
jwhong <jwhong98@gmail.com>
2023-02-22 15:02:12 -0800

    base file structure

---
+++ src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import classes from "./Main.module.css";
+import { data } from "../../data.json";
+
+const Main = () => {
+  return <main>main</main>;
+};
+
+export default Main;
