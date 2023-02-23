2c88fd23db7fb26fbbb03f99def014def00c740c
jwhong <jwhong98@gmail.com>
2023-02-22 16:05:34 -0800

    added structure and styling to CommentCard component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,14 @@
 import React from "react";
 import classes from "./Main.module.css";
 import { data } from "../../data.json";
+import CommentCard from "../CommentCard/CommentCard";
 
 const Main = () => {
-  return <main>main</main>;
+  return (
+    <main>
+      <CommentCard />
+    </main>
+  );
 };
 
 export default Main;
