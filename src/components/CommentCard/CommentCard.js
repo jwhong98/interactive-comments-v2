08d3fd6e303117a2c5f9f1bb5eda24728e0fe938
jwhong <jwhong98@gmail.com>
2023-02-27 14:16:42 -0800

    added reply component to CommentCard

--- src/components/CommentCard/CommentCard.js
+++ src/components/CommentCard/CommentCard.js
@@ -1,76 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import Badge from "../Badge/Badge";
 import classes from "./CommentCard.module.css";
 
 const CommentCard = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onReplyHandler = () => {
+    setIsOpen(!isOpen);
+  };
   const isCurrentUser =
     props.currentUser.username === props.user.username ? true : false;
-  const onClickHandler = () => {
-    props.onClick();
-  };
   return (
-    <div className={classes.commentCard}>
-      <div className={classes.commentCard__header}>
-        <img
-          src={props.user.image.webp}
-          alt="user-avatar"
-          className={classes.commentCard__header__avatar}
-        />
-        <span className={classes.commentCard__header__userName}>
-          {props.user.username}
-        </span>
-        {isCurrentUser && (
-          <span className={classes.commentCard__header__currentUserBadge}>
-            you
+    <>
+      <div className={classes.commentCard}>
+        <div className={classes.commentCard__header}>
+          <img
+            src={props.user.image.webp}
+            alt="user-avatar"
+            className={classes.commentCard__header__avatar}
+          />
+          <span className={classes.commentCard__header__userName}>
+            {props.user.username}
           </span>
-        )}
-        <span className={classes.commentCard__header__createdAt}>
-          {props.createdAt}
-        </span>
-      </div>
-      <div className={classes.commentCard__comment}>
-        {props.replyingTo && (
-          <span className={classes.replyTo}>@{props.replyingTo} </span>
-        )}
-        {props.content}
-      </div>
-      <div className={classes.commentCard__footer}>
-        <Badge score={props.score} />
+          {isCurrentUser && (
+            <span className={classes.commentCard__header__currentUserBadge}>
+              you
+            </span>
+          )}
+          <span className={classes.commentCard__header__createdAt}>
+            {props.createdAt}
+          </span>
+        </div>
+        <div className={classes.commentCard__comment}>
+          {props.replyingTo && (
+            <span className={classes.replyTo}>@{props.replyingTo} </span>
+          )}
+          {props.content}
+        </div>
+        <div className={classes.commentCard__footer}>
+          <Badge score={props.score} />
 
-        {isCurrentUser ? (
-          <div className={classes.buttonContainer}>
-            <button className={classes.deleteBtn} onClick={props.onClick}>
-              <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
+          {isCurrentUser ? (
+            <div className={classes.buttonContainer}>
+              <button className={classes.deleteBtn} onClick={props.onClick}>
+                <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z"
+                    fill="#ED6368"
+                  />
+                </svg>
+                Delete
+              </button>
+              <button className={classes.btn}>
+                <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z"
+                    fill="#5357B6"
+                  />
+                </svg>
+                Edit
+              </button>
+            </div>
+          ) : (
+            <button className={classes.btn} onClick={onReplyHandler}>
+              <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg">
                 <path
-                  d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z"
-                  fill="#ED6368"
-                />
-              </svg>
-              Delete
-            </button>
-            <button className={classes.btn}>
-              <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z"
+                  d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"
                   fill="#5357B6"
                 />
               </svg>
-              Edit
+              Reply
             </button>
-          </div>
-        ) : (
-          <button className={classes.btn}>
-            <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"
-                fill="#5357B6"
-              />
-            </svg>
-            Reply
-          </button>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+      {isOpen && (
+        <div className={classes.replyToComment}>
+          <img
+            className={classes.replyToComment__img}
+            src={props.currentUser.image.webp}
+            alt="user profile"
+          />
+          <textarea
+            name=""
+            id=""
+            rows="5"
+          >{`@${props.user.username}`}</textarea>
+          <button className={classes.replyToComment__button}>Reply</button>
+        </div>
+      )}
+    </>
   );
 };
 
