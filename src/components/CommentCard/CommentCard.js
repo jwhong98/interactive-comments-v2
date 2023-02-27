63ea35c8bd27c778181852f5eff6219366860397
jwhong <jwhong98@gmail.com>
2023-02-27 13:38:25 -0800

    added edit button for current user

--- src/components/CommentCard/CommentCard.js
+++ src/components/CommentCard/CommentCard.js
@@ -36,8 +36,9 @@ const CommentCard = (props) => {
       </div>
       <div className={classes.commentCard__footer}>
         <Badge score={props.score} />
-        <div className={classes.buttonContainer}>
-          {isCurrentUser && (
+
+        {isCurrentUser ? (
+          <div className={classes.buttonContainer}>
             <button className={classes.deleteBtn} onClick={props.onClick}>
               <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -47,8 +48,18 @@ const CommentCard = (props) => {
               </svg>
               Delete
             </button>
-          )}
-          <button className={classes.replyBtn}>
+            <button className={classes.btn}>
+              <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z"
+                  fill="#5357B6"
+                />
+              </svg>
+              Edit
+            </button>
+          </div>
+        ) : (
+          <button className={classes.btn}>
             <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"
@@ -57,7 +68,7 @@ const CommentCard = (props) => {
             </svg>
             Reply
           </button>
-        </div>
+        )}
       </div>
     </div>
   );
